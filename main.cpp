330f437455b0902be8504d6853c5bbeae3732f75
Drew Fletcher <crypt4489@gmail.com>
2026-06-12 17:09:20 -0400

    removed exceptions fully

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include "ApplicationLoop.h"
 #include "ProgramArgs.h"
-#include <iostream>
 int main(int argc, char **argv)
 {
-	try 
-	{
-		ProgramArgs args(argc, argv);
-		ApplicationLoop app(args);
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what();
-		return -1;
-	}
+	ProgramArgs args(argc, argv);
+	ApplicationLoop app(args);
 	return 0;
 }
 
